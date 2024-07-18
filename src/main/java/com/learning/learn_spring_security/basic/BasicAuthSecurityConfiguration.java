203f440d978e837c8f1418a20ae5ecae08aec121
SuryanshuGupta-55 <suryanshuguptaofficial@gmail.com>
2024-07-19 00:10:49 +0530

    Step=> 13 Storing User Credentials using JDBC.

--- src/main/java/com/learning/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/learning/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.learning.learn_spring_security.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -9,9 +11,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
 @Configuration
 @EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
@@ -35,7 +41,7 @@ public class BasicAuthSecurityConfiguration {
         // Because of this now login and logout page/feature has been removed.
         //http.csrf(AbstractHttpConfigurer::disable);
         http.csrf(csrf -> csrf.disable());
-
+        http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
         http.httpBasic(withDefaults());
 
         return http.build();
@@ -48,8 +54,36 @@ public class BasicAuthSecurityConfiguration {
     * LDAP : Lightweight Directory Access Protocol.
     * */
 
+//    @Bean
+//    public UserDetailsService userDetailsService(){
+//        Roles USER = Roles.USER;
+//        Roles ADMIN = Roles.ADMIN;
+//
+//        var user = User.withUsername("suryanshu")
+//                .password("{noop}dummy")
+//                .roles(USER.toString())
+//                .build();
+//        // {noop} => To not use any encoding we are using this.
+//
+//        var admin = User.withUsername("admin")
+//                .password("{noop}dummy")
+//                .roles(ADMIN.toString())
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user,admin);
+//    }
+
     @Bean
-    public UserDetailsService userDetailsService(){
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+        // The addScript points to a script users.ddl which runs and set up the database for user details.
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
         Roles USER = Roles.USER;
         Roles ADMIN = Roles.ADMIN;
 
@@ -64,6 +98,14 @@ public class BasicAuthSecurityConfiguration {
                 .roles(ADMIN.toString())
                 .build();
 
-        return new InMemoryUserDetailsManager(user,admin);
+        // Now we are using jdbcUserDetailsManager to manage
+        //return new InMemoryUserDetailsManager(user,admin);
+
+        //Jdbc user management service, based on the same table structure as its parent class, JdbcDaoImpl.
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
     }
 }
--- src/main/resources/Step.md
+++ src/main/resources/Step.md
@@ -14,3 +14,18 @@ In this step we will setup our own security chain,
 		}
 This is the default code which sets the SecurityFilterChain. We want to disable Csrf,
 and form login and make stateless api therefore we will overwrite with our own in BasicAuthSecurityConfiguration.
+
+Step => 13
+
+Will store the user details in H2 database from in-memory database.
+when we set up the H2 page will give error because of frames.
+By default, spring security disable frames.
+=> 
+`http.headers().framesOptions().sameOrigin()`
+
+The above code is deprecated use:
+
+`http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));`
+
+There is a class as JdbcDaoImpl, there is a file as well users.ddl which creates the Data Structure.
+So, we will use this to setup our database, for user details.
