5671f3d6ee08a77945800bf2f30f35999ea0464f
SuryanshuGupta-55 <suryanshuguptaofficial@gmail.com>
2024-07-19 14:13:47 +0530

    Step => 18 Setting up JWT Decoder.

--- src/main/java/com/learning/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/learning/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -1,26 +1,46 @@
-package com.learning.learn_spring_security.basic;
+package com.learning.learn_spring_security.jwt;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+import javax.sql.DataSource;
+
+import com.learning.learn_spring_security.basic.Roles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
 
-//@Configuration
+@Configuration
 @EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
-public class BasicAuthSecurityConfiguration {
+public class JwtSecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -41,36 +61,20 @@ public class BasicAuthSecurityConfiguration {
         //http.csrf(AbstractHttpConfigurer::disable);
         http.csrf(csrf -> csrf.disable());
         http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+        http.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(withDefaults())
+        );
         http.httpBasic(withDefaults());
 
         return http.build();
     }
-    
+
     // Storing User Credentials:
     /*
-    * In Memory : For test purposes. Not recommend in production (application.properties).
-    * Database : We can use JDBC/JPA to access the credentials.
-    * LDAP : Lightweight Directory Access Protocol.
-    * */
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//        Roles USER = Roles.USER;
-//        Roles ADMIN = Roles.ADMIN;
-//
-//        var user = User.withUsername("suryanshu")
-//                .password("{noop}dummy")
-//                .roles(USER.toString())
-//                .build();
-//        // {noop} => To not use any encoding we are using this.
-//
-//        var admin = User.withUsername("admin")
-//                .password("{noop}dummy")
-//                .roles(ADMIN.toString())
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user,admin);
-//    }
+     * In Memory : For test purposes. Not recommend in production (application.properties).
+     * Database : We can use JDBC/JPA to access the credentials.
+     * LDAP : Lightweight Directory Access Protocol.
+     * */
 
     @Bean
     public DataSource dataSource(){
@@ -115,5 +119,48 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Configure a Decoder for JWT Token.
+
+    // 1 . Generating a key-pair.
+    // Used java.security.KeyPairGenerator.
+    @Bean
+    public KeyPair keyPair() throws NoSuchAlgorithmException {
+        var keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
+    // 2. Create RSA key object using Key Pair.
+        // Used com.nimbusds.jose.RSAKey
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair){
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    //3. Create JWKSource(JSON Web Key Source)
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey){
+        //a . Create JWKSet (a new JSON Web Key Set) with the RSA Key.
+        var jwkSet = new JWKSet(rsaKey);
+        //b. Create JWK using the JWKSet.
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+//        var jwkSource = new JWKSource(){
+//
+//            @Override
+//            public List get(JWKSelector jwkSelector, SecurityContext securityContext) throws KeySourceException {
+//                return jwkSelector.select(null);
+//            }
+//        };
+//        return jwkSource;
+    }
+    //4 . Use RSA Public Key for Decoding.
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKey.toRSAPublicKey()).build();
     }
 }
